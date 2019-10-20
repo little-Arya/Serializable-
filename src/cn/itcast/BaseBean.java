c011bb15e8b127f0a93ba29cc2ea93673351098b
Arya <2576310176@qq.com>
2019-10-20 20:56:01 +0800

    nothing

---
+++ src/cn/itcast/BaseBean.java
@@ -0,0 +1,15 @@
+package cn.itcast;
+
+public class BaseBean {
+
+    public String property1;
+    public String property2;
+    public int property3;
+
+    public BaseBean(String id){
+
+    }
+
+    public BaseBean(){}
+
+}
